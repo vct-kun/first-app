cb727a545fdc01b2aaba0bc2867e6560def37422
Van Charles TRAN <vctran@vctran1.(none)>
2013-08-08 17:25:33 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sequana::Application.config.secret_token = 'e68ea059e316f4e3963335ae2b15ab02b6888cdf87a3660cdd52e2f0d2c6f440738a554a17068c279bb46101e93833873ad49e60bf9902dec2e157f77f84d9c8'
